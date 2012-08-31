41ffd034d6e4ebf04d34eed8580ff94975190164
John Bender <john.m.bender@gmail.com>
2012-08-31 11:35:07 -0700

    todos to sort out compiled interation

--- src/core.js
+++ src/core.js
@@ -5,6 +5,7 @@ define( "core", [], function() {
 	var Wield = window.Wield = {};
 
 //>>excludeStart("exclude", pragmas.exclude);
+	// TODO not a fan of excluding the return with a pragma
 	return Wield;
 });
 //>>excludeEnd("exclude");
--- src/dom.js
+++ src/dom.js
@@ -9,6 +9,7 @@ define( "wield.dom", [ "core" ], function( Wield ) {
 	var dom = Wield.Dom.prototype = {};
 
 //>>excludeStart("exclude", pragmas.exclude);
+	// TODO not a fan of excluding the return with a pragma
 	return dom;
 });
 //>>excludeEnd("exclude");
