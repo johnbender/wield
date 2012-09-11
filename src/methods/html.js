2ead7827aa89812ec13f43378c69f49c6cfa5e89
John Bender <john.m.bender@gmail.com>
2012-09-10 22:50:35 -0700

    note on the lack of string -> html functionality in Wield.Dom.prototype.html

--- src/methods/html.js
+++ src/methods/html.js
@@ -2,6 +2,10 @@
 define([ "wield.dom" ], function( dom ) {
 //>>excludeEnd("exclude");
 
+	// NOTE this method absolutely punts on creating html from
+	//      strings. It seems all together too likely that anyone
+	//      who might use this library would be knowledgeable enough
+	//      to manage creating and appending elements appropriately
 	dom.html = function( el ) {
 		// NOTE see readme about invocation patterns
 		var e = el ? el : this._e;
