4c1b742d90c37f739a8073462e2471097e7dc04f
John Bender <john.m.bender@gmail.com>
2012-09-12 12:27:13 -0700

    make sure the jquery testing build doesn't bork because the Wield definition of html is read only

--- src/jquery.js
+++ src/jquery.js
@@ -4,6 +4,11 @@ define( "wield.jquery", ["core"], function() {
 		require( ["methods/methods"], function() {
 //>>excludeEnd("exclude");
 			/*global jQuery, dom*/
+
+			// move Wield's html as it's read only
+			dom.getHtml = dom.html;
+			dom.html = undefined;
+
 			jQuery.dom = jQuery.extend( jQuery.dom, dom );
 //>>excludeStart("exclude", pragmas.exclude);
 		});
