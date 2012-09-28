d7d7a133d7c0fae57fbb01aee7f67654960ca21f
John Bender <john.m.bender@gmail.com>
2012-09-28 16:39:07 -0700

    clean up trim

--- src/methods/trim.js
+++ src/methods/trim.js
@@ -3,15 +3,17 @@ define([ "methods" ], function( methods ) {
 //>>excludeEnd("exclude");
 	var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, useBrowserTrim;
 
-	useBrowserTrim =  String.prototype.trim && !"\uFEFF\xA0".trim();
+	useBrowserTrim = String.prototype.trim && !"\uFEFF\xA0".trim();
 
-	methods.trim = useBrowserTrim ?
-		function( string ) {
+	if( useBrowserTrim ) {
+		methods.trim = function( string ) {
 			return string.trim();
-		} :
-	function( text ) {
-		return text.toString().replace( rtrim, "" );
-	};
+		};
+	} else {
+		methods.trim = function( string ) {
+			return string.toString().replace( rtrim, "" );
+		};
+	}
 //>>excludeStart("exclude", pragmas.exclude);
 });
 //>>excludeEnd("exclude");
