9c5a0c2f06d85a1f2ae38287637414c2348cb6ea
John Bender <john.m.bender@gmail.com>
2012-09-29 23:10:33 -0700

    add some much needed clarity to dom.js

--- src/dom.js
+++ src/dom.js
@@ -12,10 +12,21 @@ define([ "core", "methods", "methods/all" ], function( Wield, methods ) {
 		return add && add._e ? add._e : add;
 	};
 
+	// We use a function to avoid the value capture issue
 	function defineMethod( name, make ) {
 		dom[ name ] = make( methods[name] );
 	}
 
+	// NOTE for all the methods defined on out method object
+	//      we want to define an equivalent on the Wield.Dom
+	//      prototype.
+	//
+	//      Methods of length one only take anelement argument
+	//      so we wrap them and pass in the _e property.
+	//
+	//      Methods of length two take a second argument that
+	//      can either be a dom object or a wield wrapper of the
+	//      same.
 	for( method in methods ) {
 		fn = methods[method];
 
@@ -38,8 +49,14 @@ define([ "core", "methods", "methods/all" ], function( Wield, methods ) {
 		}
 	}
 
+	// NOTE for each of the following methods we want to
+	//      return a string value and not the Wield.Dom
+	//      "instance"
+	//
+	//      Because toElem ignores anything that's not
+	//      a wield object (checks for _e prop) that arg
+	//      can just as easily be as string (eg, text)
 	stringReturns = [ "text", "html", "prop", "attr" ];
-
 	l = stringReturns.length;
 
 	while( l-- ) {
