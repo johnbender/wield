099e144296813b98a284fea6c9f7dec3fb5e7610
John Bender <john.m.bender@gmail.com>
2012-09-29 23:59:12 -0700

    remove attr fix ref

--- src/methods/attr.js
+++ src/methods/attr.js
@@ -1,11 +1,11 @@
 //>>excludeStart("exclude", pragmas.exclude);
-define([ "methods" ], function( methods ) {
+define([ "methods", "methods/prop" ], function( methods ) {
 //>>excludeEnd("exclude");
 	// NOTE xml documents not supported, downcasing attributes
 	//      also not supported
 	methods.attr = function( elem, name, value ) {
 		// NOTE see readme about invocation patterns
-		var nodeType = elem.nodeType, ret, fix = methods.attr.fix;
+		var nodeType = elem.nodeType, ret;
 
 		// don't get/set attributes on text, comment and attribute nodes
 		if ( !elem || nodeType === 3 || nodeType === 8 || nodeType === 2 ) {
